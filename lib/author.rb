34bf07daa7112ebc8e1b8193cad8e14e2ce6b0db
Carrie Savinell <carrielane89@gmail.com>
2018-05-24 13:46:01 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,6 @@
 class Author 
   
-  post_counter = 0 
+  @@post_counter = 0 
   attr_accessor :name, :posts 
   
   def initialize(name)
@@ -10,7 +10,7 @@ class Author
   
   def add_post(a_post)
     @posts << a_post 
-    post_counter += 1 
+    @@post_counter += 1 
     a_post.author = self 
   end   
   
